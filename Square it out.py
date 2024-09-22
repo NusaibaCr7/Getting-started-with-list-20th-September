208404fea201d70db303b1dafefad2fa1dbd7a1b
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-22 17:45:09 +0600

    Real Madrid

---
+++ Square it out.py
@@ -0,0 +1,13 @@
+def sq(l):
+    square = []
+    for i in l:
+        square.append(i*i)
+        
+    return square
+
+li = [4,5,1,2,9,7,10,8]
+print("Original List: ", li)
+
+res = sq(li)
+
+print(f"Square list: {res}")
